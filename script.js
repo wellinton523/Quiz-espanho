d33fabf5fd10ae976de164144c3a7b364e1008e4
weto <91817499+wellinton523@users.noreply.github.com>
2026-06-05 11:59:15 -0300

    Add initial question structure and ChangeQuestion function

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const questoes = {
+  "1":{
+    "texto":false,
+    "respostas":4,
+    "pergunta":"Como é chamado cachorro em espanho?",
+    "1":"Perro",
+    "2":"Cacholo",
+    "3":"Dog",
+    "4":"nenhuma da anteriores"
+  }
+}
+
+function ChangeQuestion(questao){
+  var feitas = {}
+
+  if feitas.
+  
+}
